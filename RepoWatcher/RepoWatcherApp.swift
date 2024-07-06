d1841a4303ce2bbc73b549b18e81c26fa92b3351
mohammad blur <blor1379@gmail.com>
2024-07-06 23:03:52 +0330

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Mohammad Blur on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

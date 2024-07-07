b3737bef50dfe6e0b4ed8bb715d00def56b4c6e5
mohammad blur <blor1379@gmail.com>
2024-07-07 22:30:30 +0330

    Create RepoWatcherWidgetBundle.swift

---
+++ RepoWatcherWidget/RepoWatcherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RepoWatcherWidgetBundle.swift
+//  RepoWatcherWidget
+//
+//  Created by Mohammad Blur on 7/7/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RepoWatcherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RepoWatcherWidget()
+    }
+}

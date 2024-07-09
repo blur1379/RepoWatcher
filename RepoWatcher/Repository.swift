15bbfe30d9418d3ea00e2cd3dd2866e57bd28773
mohammad blur <blor1379@gmail.com>
2024-07-09 23:41:56 +0330

    Update Repository.swift

--- RepoWatcher/Repository.swift
+++ RepoWatcher/Repository.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
-struct Repository: Codable {
+struct Repository: Decodable {
+    let neme: String
+    let owner: Owner
+    let hasIssues: Bool
+    let forks: Int
+    let watchers: Int
+    let openIssues: Int
+    let pushedAt: String
     
+}
+
+struct Owner: Decodable {
+    let avatarUrl: String
 }

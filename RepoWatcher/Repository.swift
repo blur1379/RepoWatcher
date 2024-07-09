0bf91e3dd029db6e87be9d40c5b3938499168e3d
mohammad blur <blor1379@gmail.com>
2024-07-09 23:50:05 +0330

    Update Repository.swift

--- RepoWatcher/Repository.swift
+++ RepoWatcher/Repository.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Repository: Decodable {
-    let neme: String
+    let name: String
     let owner: Owner
     let hasIssues: Bool
     let forks: Int
@@ -16,6 +16,17 @@ struct Repository: Decodable {
     let openIssues: Int
     let pushedAt: String
     
+    static let placeHolder = Repository(
+        name: "your repo",
+        owner: Owner(
+            avatarUrl: ""
+        ),
+        hasIssues: true,
+        forks: 65,
+        watchers: 123,
+        openIssues: 55,
+        pushedAt: "2022-08-09T18:19:30Z"
+    )
 }
 
 struct Owner: Decodable {

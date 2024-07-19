b60d7dbaf61079c9c7acc9092674c4dd0478eafb
mohammad blur <blor1379@gmail.com>
2024-07-19 17:00:05 +0330

    :sparkles: add group

--- RepoWatcher/ContentView.swift
+++ RepoWatcher/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newRepo = ""
+    @State private var repos: [String] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("Ex. sallen0400/swift-news", text: $newRepo)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .textFieldStyle(.roundedBorder)
+
+                    Button {
+                        if !repos.contains(newRepo) && !newRepo.isEmpty {
+                            repos.append(newRepo)
+                            UserDefaults.shared.set(repos, forKey: UserDefaults.repoKey)
+                            newRepo = ""
+                        } else {
+                            print("repo already exist or name is empty.")
+                        }
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.green)
+                    }
+                }
+                .padding()
+
+                VStack(alignment: .leading) {
+                    Text("Saved Repos")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                        .padding(.leading)
+
+                    List(repos, id: \.self) { repo in
+                        Text(repo)
+                            .swipeActions {
+                                Button("Delete") {
+                                    if repo.count > 1 {
+                                        repos.removeAll {$0 == repo}
+                                        UserDefaults.shared.set(repos, forKey: UserDefaults.repoKey)
+                                    }
+                                }
+                                .tint(.red)
+                            }
+                    }
+                }
+            }
+            .navigationTitle("Repo List")
+            .onAppear {
+                guard let retrievedRepos = UserDefaults.shared.array(forKey: UserDefaults.repoKey) as? [String] else {
+                    let defaultValue = ["sallen0400/swift-news"]
+                    UserDefaults.shared.set(defaultValue, forKey: UserDefaults.repoKey)
+                    repos = defaultValue
+                    return }
+                repos  = retrievedRepos
+            }
         }
-        .padding()
     }
 }
 

ab5cd9bbc6483ff8e71d8c770525096ed14c54b8
mohammad blur <blor1379@gmail.com>
2024-07-22 23:25:51 +0330

    Update IntentHandler.swift

--- RepoWatcherIntents/IntentHandler.swift
+++ RepoWatcherIntents/IntentHandler.swift
@@ -10,18 +10,21 @@ import Intents
 
 class IntentHandler: INExtension{
     
-    override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        
+    override func handler(for intent: INIntent) -> Any {        
         return self
     }
 }
 
 extension IntentHandler: SelectSingleRepoIntentHandling {
     func provideRepoOptionsCollection(for intent: SelectSingleRepoIntent) async throws -> INObjectCollection<NSString> {
-        guard let repos = UserDefaults.shared.value(forKey: UserDefaults.repoKey) else {
+        guard let repos = UserDefaults.shared.value(forKey: UserDefaults.repoKey) as? [String] else {
             throw UserDefaultsError.retrieval
         }
+        
+        return INObjectCollection(items: repos as [NSString])
+    }
+    
+    func defaultRepo(for intent: SelectSingleRepoIntent) -> String? {
+        return "blur1379/RepoWatcher"
     }
 }
